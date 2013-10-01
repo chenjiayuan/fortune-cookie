4ad69739a1acb1bff571e9e51320eb734dc45f07
Jiayuan Chen <chenjiayuancz@gmail.com>
2013-09-30 22:30:34 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yannan::Application.config.secret_token = 'e2f6cdb109545d07e0fba9d7c5cd1a94d5f7863c34e466a799ad87a9fc1cbaa0540b98906fc25323eb9f730daeac5471656a1cbfb89b0066ab32db2c1d073e46'
